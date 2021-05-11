292bf77469271e5fd38300d00412837af2928b18
Cleberson Gilvan <cleber.gilvan@gmail.com>
2021-05-11 13:33:18 -0300

    [#04] - State with setState

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -3,7 +3,7 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+// import 'package:flutter/services.dart';
 
 class AddTaskScreen extends StatelessWidget {
   @override
--- lib/widgets/task_tile_widget.dart
+++ lib/widgets/task_tile_widget.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_list/widgets/task_checkbox_widget.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('Lista de tarefas'),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (value) {},
+      title: Text(
+        'Lista de tarefas',
+        style: TextStyle(
+          decoration: isChecked ? TextDecoration.lineThrough : null,
+        ),
+      ),
+      trailing: TaskCheckbox(
+        checkboxState: isChecked,
+        toggleCheckboxState: (bool? checkboxState) {
+          setState(() {
+            isChecked = checkboxState!;
+          });
+        },
       ),
     );
   }
